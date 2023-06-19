7a409a99f9f08953c050c77ad4b5ceac8723b766
Dhruva Srinivas <dhruvas17068@gmail.com>
2023-06-19 15:13:59 +0530

    feat: auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import { Button } from "~/components/ui/button";
 import { api } from "~/utils/api";
 
 export default function Home() {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  const { data: sessionData } = useSession();
 
   return (
     <>
@@ -15,9 +15,25 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <div className="flex justify-center">
-          <Button>hello</Button>
+        <div className="flex justify-center pt-5">
+          {sessionData ? (
+            <div>
+              <Button onClick={() => void signOut()} variant="outline">
+                sign out
+              </Button>
+            </div>
+          ) : (
+            <Button onClick={() => void signIn()} variant="outline">
+              sign in
+            </Button>
+          )}
         </div>
+
+        {sessionData && (
+          <p className="flex justify-center leading-7 [&:not(:first-child)]:mt-6">
+            Signed in User: <span className="pl-1 font-bold">{sessionData?.user.name}</span>
+          </p>
+        )}
       </main>
     </>
   );
